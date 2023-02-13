efd206f1e2a79e0febe2bfc1bbef682999c75cf7
임강호 <55732952+Icln@users.noreply.github.com>
2023-02-13 17:07:15 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 252 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+void star(int i, int j, int n) {
+	if ((i / n) % 3 == 1 && (j / n) % 3 == 1)
+	{
+		cout << ' ';
+	}
+	else
+	{
+		if (n / 3 == 0)
+		{
+			cout << "*";
+		}
+		else
+		{
+			star(i, j, n / 3);
+		}
+	}
+}
+int main() {
+	ios::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			star(i, j, n);
+		}
+		cout << "\n";
+	}
+	return 0;
+}
