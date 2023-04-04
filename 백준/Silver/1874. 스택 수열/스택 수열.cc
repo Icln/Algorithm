161c11483783bb27bf9367b88342bf140fc2bfe2
임강호 <55732952+Icln@users.noreply.github.com>
2023-04-04 13:18:08 +0900

    [Silver II] Title: 스택 수열, Time: 20 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,33 +1,33 @@
 #include<iostream>
 #include<stack>
 #include<vector>
-
 using namespace std;
-int main(){
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
     stack<int> s;
     vector<char> result;
     int cnt = 1;
     int n;
     cin >> n;
-    
-    for(int i = 0; i < n; i++){
+
+    for (int i = 0; i < n; i++) {
         int x;
         cin >> x;
-        while(cnt <= x){
+        while (cnt <= x) {
             s.push(cnt);
-            cnt+=1;
+            cnt += 1;
             result.push_back('+');
         }
-        if(s.top() == x){
+        if (s.top() == x) {
             s.pop();
             result.push_back('-');
         }
-        else{
-            cout <<"NO";
+        else {
+            cout << "NO";
             return 0;
         }
     }
-    for(int i =0; i< result.size(); i++){
-        cout << result[i] <<'\n';
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << '\n';
     }
 }
