857f5b320a4586600bcda990a3c7ebedf09595af
임강호 <55732952+Icln@users.noreply.github.com>
2023-05-18 15:49:54 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,20 @@
+a = input()
+result = 0
+for i in a:
+   if i == 'A' or i == 'B' or i == 'C':
+      result +=3
+   elif i == 'D' or i == 'E' or i == 'F':
+      result +=4
+   elif i == 'G' or i == 'H' or i == 'I':
+      result +=5
+   elif i == 'J' or i == 'K' or i == 'L':
+      result +=6
+   elif i == 'M' or i == 'N' or i == 'O':
+      result +=7
+   elif i == 'P' or i == 'Q' or i == 'R' or i == 'S':
+      result +=8
+   elif i == 'T' or i == 'U' or i == 'V':
+      result +=9
+   elif i == 'W' or i == 'X' or i == 'Y' or i == 'Z':
+      result +=10
+print(result)
