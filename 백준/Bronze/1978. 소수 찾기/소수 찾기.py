b430cffe48664d44e76de5cfa6c62e78ed854c35
임강호 <55732952+Icln@users.noreply.github.com>
2023-05-22 01:30:35 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -3,16 +3,12 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 result = 0
-
 for i in arr:
     check = True
     if i == 1:
         pass 
     elif i == 2:
         result+=1
-        pass
-    elif i == 3:
-        result +=1
         pass
     elif i % 2 == 0:
         pass
