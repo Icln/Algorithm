0f4c16b6854259daff28d19cbb66de73fb03c686
임강호 <55732952+Icln@users.noreply.github.com>
2023-05-24 03:16:10 +0900

    [Bronze II] Title: 분해합, Time: 1192 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,15 +1,11 @@
-arr = []
-n = int(input())
-for i in range(n) : 
-    tmp = i
-    
-    for j in str(i):
-        tmp+= int(j)
-    
-    if tmp == n:
-        arr.append(i)
+n = int(input()) 
 
-if len(arr) == 0:
-    print(0)
-else :
-    print(min(arr))
+for i in range(1,n+1): 
+    num = sum((map(int, str(i)))) 
+    num_sum = i + num  
+    
+    if num_sum == n:
+        print(i)
+        break
+    if i == n:  
+        print(0)
