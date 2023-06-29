36b63e123929c0eb54d64f1c3d051c4e35cce179
임강호 <55732952+Icln@users.noreply.github.com>
2023-06-30 02:25:16 +0900

    [Silver III] Title: 소수 구하기, Time: 1716 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -2,26 +2,20 @@ import sys
 import math
 input = sys.stdin.readline
 
-def check(x):
-    for i in range(2, int(math.sqrt(x)) + 1):
-        if x % i == 0:
-            return False
-    return True
-
 def prime(x):
-    if x < 2:
+    if x == 1:
         return False
     if x == 2:
         return True
-    if x % 2 == 0:
+    elif x % 2 == 0:
         return False
-    else:
-        if (check(x)):
-            return True
-        else:
-            return False
+    else: 
+        for i in range(3, int(math.sqrt(x)) + 1, 2):
+            if x % i == 0:
+                return False
+        return True
+    
 x, y = map(int, input().split())
-
 for i in range(x, y+1):
-    if(prime(i)):
+    if prime(i):
         print(i)
