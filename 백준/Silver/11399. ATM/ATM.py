07a7dbce84fa6e44976fe13df57c6e72355c8716
임강호 <55732952+Icln@users.noreply.github.com>
2023-07-20 18:05:33 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,9 +1,10 @@
-n = int(input())  # 첫째줄 입력
-peoples = list(map(int, input().split()))  # 기다리는 사람들 리스트 형태로 입력 받음
+import sys
+input = sys.stdin.readline
 
-peoples.sort()  # 작은 순서대로 정렬
-answer = 0  # 정답 변수를 0으로 만듭니다.
+n = int(input())
+arr = sorted(list(map(int, input().split())))
 
-for x in range(1, n+1):
-    answer += sum(peoples[0:x])  # 리스트의 0번째 수부터 x번째 수까지를 더해줍니다.
-print(answer)  # 정답 제출
+for i in range(1, n):
+    arr[i] += arr[i - 1]
+
+print(sum(arr))
