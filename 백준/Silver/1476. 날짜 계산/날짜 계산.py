02510a58d425b3df852c318ce6af2f42a52b8a5d
임강호 <55732952+Icln@users.noreply.github.com>
2023-07-25 22:05:46 +0900

    [Silver V] Title: 날짜 계산, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,9 @@
+E, S, M = map(int, input().split())
+year = 1
+
+while True:
+    if ((year-E) % 15 == 0) and ((year-S) % 28 == 0) and ((year-M) % 19 == 0):
+        break 
+    year += 1
+
+print(year)
