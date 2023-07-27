0f45cd93b211c709297c4c794cbc7c7951c62e85
임강호 <55732952+Icln@users.noreply.github.com>
2023-07-27 11:42:44 +0900

    [Silver II] Title: 나무 자르기, Time: 3400 ms, Memory: 143400 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -2,18 +2,21 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-trees = list(map(int, input().split()))
-start, end = 1, max(trees)
+trees = sorted(list(map(int, input().split())))
+start, end = 1, trees[-1]
 
 while start <= end:
     h = (start + end) // 2
     num = 0
     for tree in trees:
-        if tree >= h:
+        if tree > h:
             num += (tree - h)
+        if num > m:
+            break
+
     if num >= m:
         start = h + 1
     else:
         end = h - 1
-
+    
 print(end)
