dc49179805376fe09ad1b0bdbb927f91ba6613e0
임강호 <55732952+Icln@users.noreply.github.com>
2023-08-03 17:12:21 +0900

    [Gold III] Title: 양팔저울, Time: 76 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.py
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -5,20 +5,23 @@ w = int(input())
 weights = list(map(int, input().split()))
 m = int(input())
 marbles = list(map(int, input().split()))
-dp = [False for i in range(40001)]
+dp = [False for i in range(15001)]
 dp[0] = True
 
 for weight in weights:
     tmp = set()
-    for i in range(40001):
+    for i in range(15001):
         if dp[i]:
             tmp.add(i + weight)
             tmp.add(abs(i - weight))
     for j in tmp:
-        dp[j] = True
+        if j <= 15000:
+            dp[j] = True
     
 for i in marbles:
-    if dp[i]:
+    if i > 15000:
+        print('N', end=' ')
+    elif dp[i]:
         print('Y', end=' ')
     else:
         print('N', end=' ')
