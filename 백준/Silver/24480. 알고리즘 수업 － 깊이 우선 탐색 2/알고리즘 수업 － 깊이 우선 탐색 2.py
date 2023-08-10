08661d51ee8c02b399005955f0b226f623a48d89
임강호 <55732952+Icln@users.noreply.github.com>
2023-08-10 11:48:40 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 680 ms, Memory: 68132 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,28 @@
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+def dfs(u):
+    global cnt
+    visited[u] = cnt
+    for i in graph[u]:
+        if visited[i] == 0:
+            cnt +=1 
+            dfs(i) 
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+cnt = 1
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(1, n + 1):
+    graph[i].sort(reverse = True)
+
+dfs(r)
+for i in range(1, n + 1):
+    print(visited[i])
