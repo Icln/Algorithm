f7f677cc16d6ce05e44b518bf295c768fd3a9999
임강호 <55732952+Icln@users.noreply.github.com>
2023-08-10 12:13:22 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 552 ms, Memory: 58420 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,34 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def bfs(u):
+    global cnt
+    visited[u] = cnt
+    queue = deque([u])
+
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if visited[i] == 0:
+                queue.append(i)
+                cnt +=1 
+                visited[i]= cnt
+             
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+cnt = 1
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(1, n + 1):
+    graph[i].sort()
+
+bfs(r)
+for i in range(1, n + 1):
+    print(visited[i])
