25b21c071bbe50fedeed968e08fdbbda7e8f08ca
임강호 <55732952+Icln@users.noreply.github.com>
2023-08-22 21:20:55 +0900

    [level 2] Title: 피보나치 수, Time: 0.15 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,17 @@
+def solution(n):
+    return fibo(n) % p
+
+dp = dict()
+p = 1234567
+dp[0], dp[1], dp[2] = 0, 1, 1
+def fibo(n):
+    if dp.get(n) != None:
+        return dp[n]
+    if n % 2 == 0:
+        dp[n // 2 + 1] = fibo(n // 2 + 1) % p 
+        dp[n // 2 - 1] = fibo(n // 2 - 1) % p
+        return (dp[n // 2 + 1] ** 2 % p) - (dp[n // 2 - 1] ** 2 % p)
+    else:
+        dp[n // 2 + 1] = fibo(n // 2 + 1) % p
+        dp[n // 2] = fibo(n // 2) % p
+        return (dp[n // 2 + 1] ** 2 % p) + (dp[n // 2] ** 2 % p)
