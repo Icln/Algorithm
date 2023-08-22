97a117b064d7ff1cc4aa5332fe50c7af7f9353a2
임강호 <55732952+Icln@users.noreply.github.com>
2023-08-22 20:28:10 +0900

    [level 2] Title: 숫자의 표현, Time: 3.05 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,13 @@
+def solution(n):
+    arr = [0] * (n + 1)
+    answer = 0
+    for i in range(1, (n // 2) + 1):
+        plus = 1
+        tmp = i
+        while tmp <= n:
+            if tmp == n:
+                answer += 1
+                break
+            tmp += (i + plus) 
+            plus += 1
+    return answer + 1
