7f3dd4749312a3d19736920171874767a4a1ef84
임강호 <55732952+Icln@users.noreply.github.com>
2023-08-29 14:43:25 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,29 +1,22 @@
 def solution(str1, str2):
-    str1, str2 = str1.upper(), str2.upper()
-    s1 = [str1[i] + str1[i + 1] for i in range(len(str1) - 1) 
-          if 'A'<= str1[i] <= 'Z' and 'A'<= str1[i + 1] <= 'Z']
-    s2 = [str2[i] + str2[i + 1] for i in range(len(str2) - 1) 
-          if 'A'<= str2[i] <= 'Z' and 'A'<= str2[i + 1] <= 'Z']
+    s1 = [str1[i: i + 2].upper() for i in range(len(str1) - 1) if str1[i: i + 2].isalpha()]
+    s2 = [str2[i: i + 2].upper() for i in range(len(str2) - 1) if str2[i: i + 2].isalpha()]
     
     union, intersection = 0, 0
-    if len(s1) == 0 and len(s2) == 0:
+    unionArr = list(set(s1) | set(s2))
+    interArr = list(set(s1) & set(s2))
+    if len(unionArr) == 0:
         return 65536
-    elif set(s1) == set(s2) and len(set(s1)) == 1:
+    elif len(interArr) == 1:
         intersection = min(len(s1), len(s2))
         union = max(len(s1), len(s2))
     elif len(s1) != len(set(s1)) or len(s2) != len(set(s2)):
-        arr = list(set(s1) & set(s2))
-        for i in arr:
+        for i in interArr:
             intersection += min(s1.count(i), s2.count(i))   
-        
-        arr = list(set(s1) | set(s2))
-        for i in arr:
-            union += max(s1.count(i), s2.count(i))   
-    
-        print(intersection, union)
+        for i in unionArr:
+            union += max(s1.count(i), s2.count(i))     
     else:
-        print(4)
-        intersection = len(set(s1) & set(s2))            
-        union = len(set(s1) | set(s2)) 
+        intersection = len(interArr)            
+        union = len(unionArr) 
     return int(intersection / union * 65536)
     
