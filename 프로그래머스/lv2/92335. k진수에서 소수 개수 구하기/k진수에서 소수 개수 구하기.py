5d36ae42a5da4f21960c0084228b2ec4eacdd055
임강호 <55732952+Icln@users.noreply.github.com>
2023-08-30 15:36:52 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -0,0 +1,35 @@
+import math
+def solution(n, k):
+    nString = jinsu(n,k)
+    answer = 0
+    
+    for i in nString.split('0'):
+        if i.isdigit():
+            print(int(i))
+            if check(int(i)):
+                answer += 1
+    
+    return answer
+
+def jinsu(n, k):
+    if n == 10:
+        return str(n)
+    tmp = ''
+    while n >= k:
+        tmp += str(n % k)
+        n //= k
+    return ''.join(reversed(tmp + str(n % k)))
+    
+def check(n):
+    if n == 1:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0 and n // i == 1:
+            return True
+        if n % i == 0 and n // i != 1:
+            return False
+    return True
