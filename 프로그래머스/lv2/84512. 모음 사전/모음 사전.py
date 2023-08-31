0b742cae180cb2a724baa93d6ae06b3623bb4e41
임강호 <55732952+Icln@users.noreply.github.com>
2023-08-31 15:48:00 +0900

    [level 2] Title: 모음 사전, Time: 4.10 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -1,9 +1,8 @@
 from itertools import product
 def solution(word):
-    gather = set(['A','E','I','O','U'])
     alpha = set()
     for i in range(5, 0, -1):
-        for j in product(gather, repeat = i):
+        for j in product("AEIOU", repeat = i):
             alpha.add(''.join(map(str, j)))
     
     alpha = sorted(list(alpha))
