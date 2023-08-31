a25029ca4b90972cba88d85b2c5750b783e38e24
임강호 <55732952+Icln@users.noreply.github.com>
2023-09-01 01:14:17 +0900

    [level 2] Title: 주식가격, Time: 19.30 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,11 +1,11 @@
 def solution(prices):
-    answer = []
+    stack = []
+    answer = [0] * len(prices)
     for i in range(len(prices)):
-        tmp = 0
-        for j in range(i + 1, len(prices)):
-            tmp += 1
-            if prices[i] > prices[j]:
-                break
-            
-        answer.append(tmp)
+        while stack != [] and stack[-1][1] > prices[i]:
+            past, _ = stack.pop()
+            answer[past] = i - past
+        stack.append([i, prices[i]])
+    for i, s in stack:
+        answer[i] = len(prices) - 1 - i
     return answer
