ed0ab9e908ff3cbb29cb99d7cc520e30bb67ef76
임강호 <55732952+Icln@users.noreply.github.com>
2023-09-01 16:34:58 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 10.06 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,29 +1,14 @@
+def extract(file):
+    head, number, tail = '', '', ''
+    for char in file:
+        if not number and not char.isdigit():
+            head += char
+        elif not tail and char.isdigit():
+            number += char
+        else:
+            tail += char
+    return [head.upper(), int(number), tail]
+
 def solution(files):
-    s = []
-    answer = []
-    for i in files:
-        head, number, tail = '', '', ''
-        for j in i:
-            if number == '' and not j.isdigit():
-                head += j
-            elif tail == '' and j.isdigit():
-                number += j
-            else:
-                tail += j
-        s.append([head.upper(), int(number), tail, files.index(i)])
-    s.sort(key = lambda x : (x[0], x[1], x[3]))
-    
-    for i in range(len(files)):
-        head, number, tail = '', '', ''
-        for j in files[i]:
-            if number == '' and not j.isdigit():
-                head += j
-            elif tail == '' and j.isdigit():
-                number += j
-            else:
-                tail += j
-        find = [head.upper(), int(number), tail, i]
-        answer.append([s.index(find), files[i]]) 
-    
-    answer.sort(key = lambda x: x[0])
-    return [i[1] for i in answer]
+    sorted_files = sorted(files, key = lambda x: (extract(x)[0], extract(x)[1], files.index(x)))
+    return sorted_files
