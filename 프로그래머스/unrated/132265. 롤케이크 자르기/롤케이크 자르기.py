1d284d0176832ce047d9820173375acd33d5640e
임강호 <55732952+Icln@users.noreply.github.com>
2023-09-04 14:37:36 +0900

    [unrated] Title: 롤케이크 자르기, Time: 466.04 ms, Memory: 64.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,23 @@
+def solution(topping):
+    kind = set(topping)
+    Front = [1] * len(topping)
+    Back = [0] * len(topping)
+    answer = 0
+    cnt = 0
+    
+    for i in range(len(topping)):
+        if topping[i] in kind:
+            cnt += 1
+            kind.remove(topping[i])
+        Front[i] = cnt
+    
+    kind = set()
+    for i in range(len(topping) - 1, -1, -1):
+        Back[i] = len(kind)
+        kind.add(topping.pop())
+        
+    for i in range(len(Front)):
+        if Front[i] == Back[i]:
+            answer += 1
+            
+    return answer
