5fae51af6905fd0ee545c1709ef1f48dd98079f4
임강호 <55732952+Icln@users.noreply.github.com>
2023-09-14 11:23:14 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.33 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -0,0 +1,26 @@
+import re
+from math import factorial
+from itertools import permutations
+def cal(x, y, op):
+    if op == '+':
+        return x + y
+    elif op == '-':
+        return x - y
+    return x * y
+def solution(expression):
+    operator = set(re.findall("[^0-9]+", expression))
+    answer = 0
+    for priority in permutations(operator):
+        numbers = list(map(int, re.split('[-+*]', expression)))
+        operators = re.findall("[^0-9]+", expression)
+        
+        for op in priority:
+            while op in operators:
+                idx = operators.index(op)
+                tmp = cal(numbers[idx], numbers[idx + 1], op)
+                numbers[idx] = tmp
+                numbers.pop(idx + 1)
+                operators.pop(idx)
+            
+        answer = max(answer, abs(numbers[0]))        
+    return answer
