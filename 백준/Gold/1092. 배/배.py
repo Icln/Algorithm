de683a65d5775888b7b90d74cd368e4a138f6e25
임강호 <55732952+Icln@users.noreply.github.com>
2023-11-26 22:01:46 +0900

    [Gold V] Title: 배, Time: 2648 ms, Memory: 110724 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+
 n = int(input())
 cranes = list(map(int, input().split()))
 m = int(input())
@@ -7,15 +8,24 @@ boxes = list(map(int, input().split()))
 
 cranes.sort(reverse=True)
 boxes.sort(reverse=True)
+
 if cranes[0] < boxes[0]:
     print(-1)
 else:
+    visited = [False] * m
     result = 0
-    while boxes:
-        for crane in cranes:
-            for box in boxes:
-                if crane >= box:
-                    boxes.remove(box)
-                    break
+    while True:
+        cnt = 0
+        idx = 0
+        for i in range(m):
+            if len(cranes) == idx:
+                break
+            if not visited[i] and cranes[idx] >= boxes[i]:
+                idx += 1
+                cnt += 1
+                visited[i] = True
+
         result += 1
+        if len(set(visited)) == 1 and visited[0]:
+            break
     print(result)
