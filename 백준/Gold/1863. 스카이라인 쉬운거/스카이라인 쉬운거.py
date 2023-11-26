a56740a7b32e2bf1f7433f9447787f5995f67631
임강호 <55732952+Icln@users.noreply.github.com>
2023-11-26 23:40:09 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 56 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
@@ -8,17 +8,12 @@ for i in range(n):
     buildings.append((x, y))
 
 result = 0
-tmp = []
+tmp = [0]
 for i in range(n):
-    if tmp and tmp[-1] > buildings[i][1]:
-        if buildings[i][1] == 0:
-            result += len(tmp)
-            tmp = []
-        else:
-            while tmp and tmp[-1] > buildings[i][1]:
-                result += 1
-                tmp.pop()
-    if buildings[i][1] != 0 and buildings[i][1] not in tmp:
+    if tmp[-1] > buildings[i][1]:
+        while tmp and tmp[-1] > buildings[i][1]:
+            result += 1
+            tmp.pop()
+    if buildings[i][1] not in tmp:
         tmp.append(buildings[i][1])
-
-print(result + len(tmp))
+print(result + (len(tmp) - 1))
