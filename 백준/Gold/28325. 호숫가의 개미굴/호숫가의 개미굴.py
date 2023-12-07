a3dcef03ba4ffae108e5663a2098112f40478270
임강호 <55732952+Icln@users.noreply.github.com>
2023-12-07 10:37:36 +0900

    [Gold V] Title: 호숫가의 개미굴, Time: 180 ms, Memory: 34036 KB, Score: 12 point -BaekjoonHub

---
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.py
@@ -0,0 +1,27 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+n = int(input())
+add = list(map(int, input().split()))
+room = deque([1 for i in range(n)])
+
+for i, j in enumerate(add):
+    if j >= 1:
+        room[i] = 0
+
+room += [room[0]]
+result = 0
+for i in range(n):
+    tmp, cnt = 0, 0
+    while cnt < n:
+        if room[cnt] == 1:
+            if cnt == n - 1 and room[n] == 1:
+                break
+            tmp += 1
+            cnt += 2
+            continue
+        cnt += 1
+    result = max(result, tmp)
+    room.rotate(1)
+
+print(result + sum(add))
