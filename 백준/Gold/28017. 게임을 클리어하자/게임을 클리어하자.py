72aece23f2a7bd12329197f3759a5399f4a4f6cd
임강호 <55732952+Icln@users.noreply.github.com>
2023-12-17 18:40:52 +0900

    [Gold V] Title: 게임을 클리어하자, Time: 2368 ms, Memory: 41304 KB -BaekjoonHub

--- 백준/Gold/28017. 게임을 클리어하자/게임을 클리어하자.py
+++ 백준/Gold/28017. 게임을 클리어하자/게임을 클리어하자.py
@@ -2,13 +2,10 @@ import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-dp = [[i for i in arr[0]]] + [[1e9] * m for _ in range(n - 1)]
 
-for i in range(1, n):
+for i in range(n - 1):
     for j in range(m):
-        for k in range(m):
-            if j == k:
-                continue
-            dp[i][j] = min(dp[i][j], dp[i - 1][k] + arr[i][j])
+        tmp = min(arr[i][:j] + arr[i][j + 1:])
+        arr[i + 1][j] += tmp
 
-print(min(dp[-1]))
+print(min(arr[-1]))
