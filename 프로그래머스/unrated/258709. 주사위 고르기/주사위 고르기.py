488d10167703da402ffe801ac6d1c817245dafb1
임강호 <55732952+Icln@users.noreply.github.com>
2024-01-07 02:59:23 +0900

    [level unrated] Title: 주사위 고르기, Time: 2090.89 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/258709. 주사위 고르기/주사위 고르기.py
@@ -0,0 +1,30 @@
+from itertools import combinations
+def cal(n, a, b, curA, curB, dice, arrA, arrB):
+    if n == len(dice) // 2:
+        arrA.append(curA)
+        arrB.append(curB)
+        return
+    for i in range(6):
+        cal(n + 1, a, b, curA + dice[a[n]][i], curB + dice[b[n]][i], dice, arrA, arrB)
+        
+def solution(dice):
+    n = len(dice)
+    case = set(i for i in range(n))
+    result = []
+    for a in combinations(case, n // 2):    
+        b = tuple(case - set(a))
+        arrA, arrB = [], []
+        cal(0, a, b, 0, 0, dice, arrA, arrB)
+        arrA.sort()
+        arrB.sort()
+        
+        cnt, cur = 0, 0
+        for tmp in arrB:
+            while cur < len(arrA) and arrA[cur] <= tmp:
+                cur += 1
+            cnt += len(arrA) - cur
+        result.append([a, cnt])
+    
+    result.sort(key=lambda x: x[1], reverse = True)
+    return [i + 1 for i in result[0][0]]
+   
