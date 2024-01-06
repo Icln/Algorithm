0fe2da6532cbbfb7e7c1d38ae78f9075294a557a
임강호 <55732952+Icln@users.noreply.github.com>
2024-01-06 23:32:53 +0900

    [level unrated] Title: 도넛과 막대 그래프, Time: 467.78 ms, Memory: 135 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -0,0 +1,21 @@
+from collections import defaultdict        
+def solution(edges):
+    start, end = defaultdict(int), defaultdict(int)
+    for edge in edges:
+        start[edge[0]] += 1
+        end[edge[1]] += 1
+    
+    v, d, s, e = 0, 0, 0, 0
+    for i in range(1, max(max(start), max(end)) + 1):
+        if start[i] == 0:
+            s += 1
+            continue
+        if start[i] >= 2 and end[i] >= 2:
+            e += 1
+            continue
+        if start[i] >= 2 and end[i] == 0:
+            v = i
+            continue
+    
+    d = start[v] - (s + e)
+    return [v, d, s, e]
