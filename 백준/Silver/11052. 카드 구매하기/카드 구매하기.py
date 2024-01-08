9cd92169e15736c5ff23ea0cd1eeb0e6511ee8fc
임강호 <55732952+Icln@users.noreply.github.com>
2024-01-08 13:44:02 +0900

    [Silver I] Title: 카드 구매하기, Time: 216 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -7,9 +7,7 @@ dp = [0] * (n + 1)
 dp[1] = p[1]
 
 for i in range(2, n + 1):
-    for j in range(1, n + 1):
-        if i < j:
-            break
+    for j in range(1, i + 1):
         dp[i] = max(dp[i], p[j] + dp[i - j])
 
 print(dp[n])
