3d7d906e3eb22df6ac15062e63b8385767a0bec0
임강호 <55732952+Icln@users.noreply.github.com>
2024-02-16 17:56:59 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 632 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -11,18 +11,20 @@ while True:
 
     arr = [list(map(int, input().split())) for _ in range(n)]
     dist = [[1e9] * n for _ in range(n)]
-    q, dist[0][0] = [], 0
+    q, dist[0][0] = [], arr[0][0]
     heapq.heappush(q, (0, 0, arr[0][0]))
 
     while q:
         x, y, w = heapq.heappop(q)
-        if x == n - 1 and y == n - 1:
-            print(f'Problem {cnt}: {dist[x][y]}')
-            break
+        if dist[x][y] < w:
+            continue
+
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             if 0 <= nx < n and 0 <= ny < n:
                 if arr[nx][ny] + w < dist[nx][ny]:
                     dist[nx][ny] = arr[nx][ny] + w
                     heapq.heappush(q, (nx, ny, dist[nx][ny]))
+
+    print(f'Problem {cnt}: {dist[-1][-1]}')
     cnt += 1
