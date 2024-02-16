be4b44ee537e49212775f27c46c913a8693c500b
임강호 <55732952+Icln@users.noreply.github.com>
2024-02-16 17:53:44 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 704 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -0,0 +1,28 @@
+import sys
+import heapq
+input = sys.stdin.readline
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+cnt = 1
+
+while True:
+    n = int(input())
+    if not n:
+        break
+
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    dist = [[1e9] * n for _ in range(n)]
+    q, dist[0][0] = [], 0
+    heapq.heappush(q, (0, 0, arr[0][0]))
+
+    while q:
+        x, y, w = heapq.heappop(q)
+        if x == n - 1 and y == n - 1:
+            print(f'Problem {cnt}: {dist[x][y]}')
+            break
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n:
+                if arr[nx][ny] + w < dist[nx][ny]:
+                    dist[nx][ny] = arr[nx][ny] + w
+                    heapq.heappush(q, (nx, ny, dist[nx][ny]))
+    cnt += 1
