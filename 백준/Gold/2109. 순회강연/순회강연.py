31dcee240fc79485683b9b4811da7ac8bf6c563a
임강호 <55732952+Icln@users.noreply.github.com>
2024-04-12 18:10:30 +0900

    [Gold III] Title: 순회강연, Time: 60 ms, Memory: 35236 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.py
+++ 백준/Gold/2109. 순회강연/순회강연.py
@@ -1,16 +1,16 @@
-import sys
+import sys, heapq
 input = sys.stdin.readline
 
 if __name__ == "__main__":
     n = int(input())
-    days = [0] * 10001
-    arr = [list(map(int, input().split())) for _ in range(n)]   
-    arr.sort(key=lambda x: -x[0])
-    
-    for i in range(n):
-        p, d = arr[i][0], arr[i][1]
-        for j in range(d, 0, -1):
-            if days[j] == 0:
-                days[j] = p
-                break
-    print(sum(days))
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    arr.sort(key=lambda x: x[1])
+
+    q = []
+    for i in arr:
+        p, d = i
+        heapq.heappush(q, p)
+        if len(q) > d:
+            heapq.heappop(q)
+
+    print(sum(q))
