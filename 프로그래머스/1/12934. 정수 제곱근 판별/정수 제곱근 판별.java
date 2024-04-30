495e04aa4dfcf451402c6ac5cefc97c151d16dde
임강호 <55732952+Icln@users.noreply.github.com>
2024-04-30 18:35:17 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(long n) {
+        if (Math.pow((int)Math.sqrt(n), 2) == n) {
+            return (long) Math.pow(Math.sqrt(n) + 1, 2);
+        }
+
+        return -1;
+
+    }
+}
