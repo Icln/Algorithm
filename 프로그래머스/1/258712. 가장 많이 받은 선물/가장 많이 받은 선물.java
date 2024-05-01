78000a7600c7b79915a361e8b3117d62c2163ce3
임강호 <55732952+Icln@users.noreply.github.com>
2024-05-01 23:26:04 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 5.86 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,40 +1,30 @@
 import java.util.*;
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        Map<String, Map<String, Integer>> giftRecords = new HashMap<>();
-        Map<String, Integer> giftScore = new HashMap<>();
-        Map<String, Integer> nextMonthGifts = new HashMap<>();
-        
-        for (String friend : friends) {
-            giftRecords.put(friend, new HashMap<>());
-            giftScore.put(friend, 0);
-            nextMonthGifts.put(friend, 0);
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < friends.length; i++) {
+            map.put(friends[i], i);
         }
-        
-        for (String gift : gifts) {
-            String[] parts = gift.split(" ");
-            String giver = parts[0];
-            String receiver = parts[1];
-            
-            giftRecords.get(giver).put(receiver, giftRecords.get(giver).getOrDefault(receiver, 0) + 1);
-            giftScore.put(giver, giftScore.get(giver) + 1);
-            giftScore.put(receiver, giftScore.get(receiver) - 1);
-        }
-        
-        for (String giver : friends) {
-            for (String receiver : friends) {
-                if (!giver.equals(receiver)) {
-                    int giftsFromGiver = giftRecords.get(giver).getOrDefault(receiver, 0);
-                    int giftsFromReceiver = giftRecords.get(receiver).getOrDefault(giver, 0);
+        int[] index = new int[friends.length];
+        int[][] record = new int[friends.length][friends.length];
 
-                    if (giftsFromGiver > giftsFromReceiver) {
-                        nextMonthGifts.put(giver, nextMonthGifts.get(giver) + 1);
-                    } else if (giftsFromGiver == giftsFromReceiver && giftScore.get(giver) > giftScore.get(receiver)) {
-                        nextMonthGifts.put(giver, nextMonthGifts.get(giver) + 1);
-                    }
-                }
-            }
+        for (String str : gifts) {
+            String[] cur = str.split(" ");
+            index[map.get(cur[0])]++;
+            index[map.get(cur[1])]--;
+            record[map.get(cur[0])][map.get(cur[1])]++;
         }
-        return Collections.max(nextMonthGifts.values());
+
+       int ans = 0;
+       for (int i = 0; i < friends.length; i++) {
+           int cnt = 0;
+           for (int j = 0; j < friends.length; j++) {
+               if(i == j) continue;
+               if (record[i][j] > record[j][i]) cnt++;
+               else if (record[i][j] == record[j][i] && index[i] > index[j]) cnt++; 
+           }
+           ans = Math.max(cnt, ans);
+        }
+        return ans;
     }
 }
