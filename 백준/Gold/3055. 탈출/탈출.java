1a17b2106a93b8c5060639b34f0a76c6887de40c
임강호 <55732952+Icln@users.noreply.github.com>
2024-05-03 02:14:28 +0900

    [Gold IV] Title: 탈출, Time: 140 ms, Memory: 14864 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -2,59 +2,42 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int INF = Integer.MAX_VALUE;
 	static int r, c;
+	static int er, ec;
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
 	static Queue<Node> q = new LinkedList<>();
 
 	static Queue<Node> w = new LinkedList<>();
-	static boolean[][] visited;
+	static int[][] dist;
 	static char[][] arr;
-	static long[][] water;
 	static class Node{
 		int x, y;
-		long cnt;
-		public Node(int x, int y, long cnt){
+		public Node(int x, int y){
 			this.x = x;
 			this.y = y;
-			this.cnt = cnt;
 		}
 	}
-	private static long bfs(){
-		while (!w.isEmpty()){
-			Node node = w.poll();
-			int x = node.x;
-			int y = node.y;
-
-			for (int i = 0; i < 4; i++){
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				if (0 <= nx && nx < r && 0 <= ny && ny < c){
-					if (arr[nx][ny] == '.' && water[nx][ny] > water[x][y] + 1){
-						water[nx][ny] = water[x][y] + 1;
-						w.add(new Node(nx, ny, water[nx][ny]));
-					}
-				}
-
-			}
-		}
-
+	private static int bfs(){
 		while (!q.isEmpty()){
 			Node node = q.poll();
 			int x = node.x;
 			int y = node.y;
-			long cnt = node.cnt;
-			if (arr[x][y] == 'D')
-				return cnt;
+			if (x == er && y == ec)
+				return dist[x][y];
 
 			for (int i = 0; i < 4; i++){
 				int nx = x + dx[i];
 				int ny = y + dy[i];
 				if (0 <= nx && nx < r && 0 <= ny && ny < c){
-					if (!visited[nx][ny] && (arr[nx][ny] == '.' || arr[nx][ny] == 'D') && (cnt < water[nx][ny])){
-						visited[nx][ny] = true;
-						q.add(new Node(nx, ny, cnt + 1));
+					if ((arr[nx][ny] == '.' || arr[nx][ny] == 'D') && arr[x][y] == 'S'){
+						arr[nx][ny] = 'S';
+						dist[nx][ny] = dist[x][y] + 1;
+						q.add(new Node(nx, ny));
+					}
+					else if ((arr[nx][ny] == '.' || arr[nx][ny] == 'S') && arr[x][y] == '*') {
+						arr[nx][ny] = '*';
+						q.add(new Node(nx, ny));
 					}
 				}
 
@@ -68,27 +51,33 @@ public class Main {
 		r = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
 		arr = new char[r][c];
-		water = new long[r][c];
-		visited = new boolean[r][c];
+		dist = new int[r][c];
 
 		for (int i = 0; i < r; i++){
 			String[] s = br.readLine().split("");
 			for (int j = 0; j < c; j++){
 				arr[i][j] = s[j].charAt(0);
-				water[i][j] = INF;
 				if (arr[i][j] == 'S') {
-					q.add(new Node(i, j, 1));
-					visited[i][j] = true;
-				} else if (arr[i][j] == '*') {
-					w.add(new Node(i, j, 0));
-					water[i][j] = 0;
+					q.add(new Node(i, j));
+				} else if (arr[i][j] == 'D') {
+					er = i;
+					ec = j;
 				}
 			}
 		}
-		long tmp = bfs();
-		if (tmp == -1)
+
+		for (int i = 0; i < r; i++){
+			for (int j = 0; j < c; j++){
+				if (arr[i][j] == '*') {
+					q.add(new Node(i, j));
+				}
+			}
+		}
+
+		int result = bfs();
+		if (result == -1)
 			System.out.println("KAKTUS");
 		else
-			System.out.println(tmp - 1);
+			System.out.println(result);
 	}
 }
