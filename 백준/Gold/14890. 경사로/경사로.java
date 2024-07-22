547b74ed6567269bbb554ab55efe5aeca00e0718
임강호 <55732952+Icln@users.noreply.github.com>
2024-07-22 17:32:44 +0900

    [Gold III] Title: 경사로, Time: 148 ms, Memory: 14944 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.java
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -19,112 +19,76 @@ public class Main {
             }
         }
 
-        //열 검사
+        // 열 검사
         for (int i = 0; i < n; i++) {
-            boolean[] visited = new boolean[n];
-            int cur = arr[0][i];
-            for (int j = 1; j < n; j++) {
-                if (Math.abs(cur - arr[j][i]) >= 2)
-                    break;
-
-                if (cur > arr[j][i] && cur - arr[j][i] == 1) { // 감소할 때
-                    int tmp = arr[j][i];
-                    boolean flag = false;
-                    for (int k = j; k < j + l; k++) {
-                        if (k >= n) { // 범위 벗어나면
-                            flag = true;
-                            break;
-                        }
-                        if (visited[k] || tmp != arr[k][i]) { // 같은 높이 아니거나 지어진적 있으면
-                            flag = true;
-                            break;
-                        }
-                        visited[k] = true;
-                    }
-                    if (flag)
-                        break;
-                    cur = arr[j][i];
-                    j += l;
-                    j--;
-                }
-
-                else if (arr[j][i] > cur && arr[j][i] - cur == 1) { // 증가할 때
-                    int tmp = cur;
-                    boolean flag = false;
-                    for (int k = j - l;  k < j; k++) {
-                        if (k < 0) { // 범위 벗어나면
-                            flag = true;
-                            break;
-                        }
-                        if (visited[k] || tmp != arr[k][i]) { // 같은 높이 아니거나 지어진적 있으면
-                            flag = true;
-                            break;
-                        }
-                        visited[k] = true;
-                    }
-                    if (flag)
-                        break;
-                    cur = arr[j][i];
-                }
-
-                if (j == n - 1)
-                    answer ++;
-            }
-        }
-        //행검사
-        for (int i = 0; i < n; i++) {
-            boolean[] visited = new boolean[n];
-            int cur = arr[i][0];
-            for (int j = 1; j < n; j++) {
-                if (Math.abs(cur - arr[i][j]) >= 2)
-                    break;
-
-                if (cur > arr[i][j] && cur - arr[i][j] == 1) { // 감소할 때
-                    int tmp = arr[i][j];
-                    boolean flag = false;
-                    for (int k = j; k < j + l; k++) {
-                        if (k >= n) { // 범위 벗어나면
-                            flag = true;
-                            break;
-                        }
-                        if (visited[k] || tmp != arr[i][k]) { // 같은 높이 아니거나 지어진적 있으면
-                            flag = true;
-                            break;
-                        }
-                        visited[k] = true;
-                    }
-                    if (flag)
-                        break;
-
-                    cur = arr[i][j];
-                    j += l;
-                    j--;
-                }
-
-                else if (arr[i][j] > cur && arr[i][j] - cur == 1) { // 증가할 때
-                    int tmp = cur;
-                    boolean flag = false;
-                    for (int k = j - l;  k < j; k++) {
-                        if (k < 0) { // 범위 벗어나면
-                            flag = true;
-                            break;
-                        }
-                        if (visited[k] || tmp != arr[i][k]) { // 같은 높이 아니거나 지어진적 있으면
-                            flag = true;
-                            break;
-                        }
-                        visited[k] = true;
-                    }
-                    if (flag)
-                        break;
-                    cur = arr[i][j];
-                }
-
-                if (j == n - 1)
-                    answer ++;
-
-            }
+            if (checkColumn(i)) 
+                answer++;
+            if (checkRow(i)) 
+                answer++;
         }
         System.out.println(answer);
+    }
+
+    private static boolean checkColumn(int col) {
+        boolean[] visited = new boolean[n];
+        int cur = arr[0][col];
+
+        for (int j = 1; j < n; j++) {
+            if (Math.abs(cur - arr[j][col]) >= 2) 
+                return false;
+
+            if (cur > arr[j][col] && cur - arr[j][col] == 1) { // 감소할 때
+                if (!decreasing(j, col, visited, true)) 
+                    return false;
+                cur = arr[j][col];
+                j += l - 1;
+            } else if (arr[j][col] > cur && arr[j][col] - cur == 1) { // 증가할 때
+                if (!increasing(j, col, visited, true)) 
+                    return false;
+                cur = arr[j][col];
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkRow(int row) {
+        boolean[] visited = new boolean[n];
+        int cur = arr[row][0];
+
+        for (int j = 1; j < n; j++) {
+            if (Math.abs(cur - arr[row][j]) >= 2) return false;
+
+            if (cur > arr[row][j] && cur - arr[row][j] == 1) { // 감소할 때
+                if (!decreasing(j, row, visited, false)) return false;
+                cur = arr[row][j];
+                j += l - 1;
+            } else if (arr[row][j] > cur && arr[row][j] - cur == 1) { // 증가할 때
+                if (!increasing(j, row, visited, false)) return false;
+                cur = arr[row][j];
+            }
+        }
+        return true;
+    }
+
+    private static boolean decreasing(int start, int fixed, boolean[] visited, boolean isColumn) {
+        int tmp = isColumn ? arr[start][fixed] : arr[fixed][start];
+        for (int k = start; k < start + l; k++) {
+            if (k >= n || visited[k] || (isColumn ? tmp != arr[k][fixed] : tmp != arr[fixed][k])) {
+                return false;
+            }
+            visited[k] = true;
+        }
+        return true;
+    }
+
+    private static boolean increasing(int start, int fixed, boolean[] visited, boolean isColumn) {
+        int tmp = isColumn ? arr[start - 1][fixed] : arr[fixed][start - 1];
+        for (int k = start - l; k < start; k++) {
+            if (k < 0 || visited[k] || (isColumn ? tmp != arr[k][fixed] : tmp != arr[fixed][k])) {
+                return false;
+            }
+            visited[k] = true;
+        }
+        return true;
     }
 }
