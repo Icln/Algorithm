ba3f8a99fd973585889db62845b4507cf8577de2
임강호 <55732952+Icln@users.noreply.github.com>
2024-08-16 11:57:41 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 166 ms, Memory: 60,280 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -1,19 +1,39 @@
-from collections import deque
+import java.io.*;
+import java.util.*;
 
-for i in range(10):
-    n = int(input())
-    arr = deque(list(map(int, input().split())))
-    flag = False
+public class Solution{
+    static ArrayDeque<Integer> arr = new ArrayDeque<>();
+    public static void encode(){
+        int num = 1;
+        while(true){
+            int tmp = arr.removeFirst() - num++;
+            if (tmp <= 0){
+                arr.addLast(0);
+                break;
+            }
+            else
+                arr.addLast(tmp);
 
-    while not flag:
-        for j in range(1, 6):
-            if min(arr) <= 0:
-                flag = True
-                break
-            if arr[0] - j < 0:
-                arr.popleft()
-                arr.append(0)
-            else:
-                arr.append(arr.popleft() - j) 
-    print(f'#{n}', *arr)
-    
+            if (num == 6)
+                num = 1;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 1; i <= 10; i++) {
+            int n = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr.clear();
+            for (int j = 0; j < 8; j++) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            
+            encode();
+            System.out.print("#" + i + " ");
+            for (Integer num : arr) {
+                System.out.printf(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
