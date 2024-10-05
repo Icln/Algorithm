ae5fea6d45a9e11bfc14e0e942ed1ebeae78e0f3
임강호 <55732952+Icln@users.noreply.github.com>
2024-10-06 01:45:48 +0900

    [Gold IV] Title: 플로이드, Time: 1024 ms, Memory: 74116 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,21 +1,18 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-	static long[][] dist;
-	static int INF = Integer.MAX_VALUE;
+class Main{
 	static int n, m;
+	static long[][] dist;
+
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		m = Integer.parseInt(br.readLine());
-		dist = new long[n][n];
-
-		for (int i = 0; i < n; i++){
-			for (int j = 0; j < n; j++){
-				if (i != j)
-					dist[i][j] = INF;
-			}
+		dist = new long[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
+			Arrays.fill(dist[i], Integer.MAX_VALUE);
+			dist[i][i] = 0;
 		}
 
 		for (int i = 0; i < m; i++){
@@ -23,27 +20,26 @@ public class Main {
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			dist[a - 1][b - 1] = Math.min(dist[a - 1][b - 1], c);
+			dist[a][b] = Math.min(dist[a][b], c);
 		}
 
-		for (int k = 0; k < n; k++){
-			for (int i = 0; i < n; i++){
-				for (int j = 0; j < n; j++){
+		for (int k = 1; k < n + 1; k++) {
+			for (int i = 1; i < n + 1; i++) {
+				for (int j = 1; j < n + 1; j++) {
 					dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
 				}
 			}
 		}
 
-		for (int i = 0; i < n; i++){
-			for (int j = 0; j < n; j++){
-				if (dist[i][j] == INF)
+		for (int i = 1; i <= n; i++){
+			for (int j = 1; j <= n; j++){
+				if (dist[i][j] == Integer.MAX_VALUE)
 					dist[i][j] = 0;
-				if (j == n - 1)
+				if (j == n)
 					System.out.printf("%d\n", dist[i][j]);
 				else
 					System.out.printf("%d ", dist[i][j]);
 			}
 		}
-
 	}
 }
