68dbac18ed2733119dd15fe0f9a179f90485d67a
임강호 <55732952+Icln@users.noreply.github.com>
2024-10-17 02:04:53 +0900

    [Gold II] Title: 미확인 도착지, Time: 460 ms, Memory: 61928 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
@@ -3,18 +3,18 @@ import java.util.*;
 
 public class Main {
 	static int T, n, m, t, s, g, h, a, b, d, x;
-	static int INF = 50000001;
+	static int INF = 50000000;
 	static int[] dist;
 	static List<Integer> answer;
 	static List<List<int[]>> graph;
 	static PriorityQueue<int[]> q;
-	public static int dijkstra(int start, int end) {
+	public static void dijkstra() {
 		q = new PriorityQueue<>((a, b) -> a[1] - b[1]);
 		dist = new int[n + 1];
 		Arrays.fill(dist, INF);
 
-		q.offer(new int[]{start, 0});
-		dist[start] = 0;
+		q.offer(new int[]{s, 0});
+		dist[s] = 0;
 		while (!q.isEmpty()) {
 			int[] tmp = q.poll();
 			int cur = tmp[0];
@@ -28,7 +28,6 @@ public class Main {
 				}
 			}
 		}
-		return dist[end];
 	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -54,23 +53,26 @@ public class Main {
 				a = Integer.parseInt(st.nextToken());
 				b = Integer.parseInt(st.nextToken());
 				d = Integer.parseInt(st.nextToken());
-				graph.get(a).add(new int[]{b, d});
-				graph.get(b).add(new int[]{a, d});
+				if((a == g && b == h) || (a == h && b == g)){
+					graph.get(a).add(new int[]{b, 2 * d - 1});
+					graph.get(b).add(new int[]{a, 2 * d - 1});
+				}
+				else{
+					graph.get(a).add(new int[]{b, 2 * d});
+					graph.get(b).add(new int[]{a, 2 * d});
+				}
 			}
 
+			dijkstra();
 			answer = new ArrayList<>();
 			for (int i = 0; i < t; i++) {
 				x = Integer.parseInt(br.readLine());
-				int tmp1 = dijkstra(s, g) + dijkstra(g, h) + dijkstra(h, x);
-				int tmp2 = dijkstra(s, h) + dijkstra(h, g) + dijkstra(g, x);
-				int tmp3 = dijkstra(s, x);
-				if (Math.min(tmp1, tmp2) == tmp3) answer.add(x);
+				if (dist[x] % 2 == 1) answer.add(x);
 			}
 
 			Collections.sort(answer);
-			for (Integer i : answer) {
+			for (Integer i : answer)
 				System.out.print(i + " ");
-			}
 			System.out.println();
 		}
 	}
