2ae3df0bb2140d44c67236558d2d71eb788c8d11
임강호 <55732952+Icln@users.noreply.github.com>
2024-10-17 19:49:05 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,6 +1,5 @@
 with recursive tmp as(
     select 0 as hour
-    from ANIMAL_OUTS
     union
     select hour + 1 as hour
     from tmp 
@@ -9,4 +8,3 @@ with recursive tmp as(
 select t.hour as hour, count(hour(datetime)) as count
 from tmp t left join ANIMAL_OUTS a on t.hour = hour(datetime)
 group by t.hour
-
