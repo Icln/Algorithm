dfe17e7d3ad0e4157db9eeb5077ca7c610b58739
임강호 <55732952+Icln@users.noreply.github.com>
2024-11-04 22:38:46 +0900

    [Gold I] Title: Gaaaaaaaaaarden, Time: 1252 ms, Memory: 317248 KB -BaekjoonHub

---
+++ 백준/Gold/18809. Gaaaaaaaaaarden/Gaaaaaaaaaarden.java
@@ -0,0 +1,122 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	static int n, m, g, r, answer;
+	static int[] red, green;
+	static int[][] arr, visitedR, visitedG;
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	static List<Integer> list = new ArrayList<>();
+	public static void combinations(int cnt, int idx, char color, HashSet<Integer> set){
+		if(color == 'R' && cnt == r){
+			combinations(0, 0, 'G', set);
+			return;
+		}
+		else if(color == 'G' && cnt == g){
+			bfs();
+			return;
+		}
+
+		if (color == 'R'){
+			for (int i = idx; i < list.size(); i++) {
+				red[cnt] = list.get(i);
+				set.add(red[cnt]);
+				combinations(cnt + 1, i + 1, 'R', set);
+				set.remove(red[cnt]);
+			}
+		}
+		else if (color == 'G'){
+			for (int i = idx; i < list.size(); i++) {
+				if (set.contains(list.get(i))) continue;
+				green[cnt] = list.get(i);
+				set.add(green[cnt]);
+				combinations(cnt + 1, i + 1, 'G', set);
+				set.remove(green[cnt]);
+			}
+		}
+
+	}
+	public static void bfs(){
+		Queue<int[]> q = new ArrayDeque<>();
+		visitedR = new int[n][m];
+		visitedG = new int[n][m];
+
+		for (int i = 0; i < r; i++) {
+			q.offer(new int[]{1, 0, red[i]});
+			visitedR[red[i]/m][red[i]%m] = 1;
+		}
+
+		for (int i = 0; i < g; i++){
+			q.offer(new int[]{1, 1, green[i]});
+			visitedG[green[i]/m][green[i]%m] = 1;
+		}
+
+		int cnt = 0;
+		while(!q.isEmpty()){
+			int[] cur = q.poll();
+			int time = cur[0];
+			int color = cur[1];
+			int x = cur[2] / m;
+			int y = cur[2] % m;
+			if (visitedR[x][y] == visitedG[x][y]){
+				continue;
+			}
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i], ny = y + dy[i];
+				if (nx < 0 || nx >= n || ny < 0 || ny >= m || arr[nx][ny] == 0) continue;
+				if (color == 0){ //  빨강
+					if (visitedR[nx][ny] > 0) continue;
+					if (visitedG[nx][ny] > 0 && visitedG[nx][ny] <= time) continue;
+
+					visitedR[nx][ny] = time + 1;
+					if(visitedG[nx][ny] == time + 1){
+						cnt++;
+						continue;
+					}
+					q.offer(new int[]{time + 1, color, nx * m + ny});
+				}
+				else{ // 초록
+					if (visitedG[nx][ny] > 0) continue;
+					if (visitedR[nx][ny] > 0 && visitedR[nx][ny] <= time) continue;
+
+					visitedG[nx][ny] = time + 1;
+					if(visitedR[nx][ny] == time + 1){
+						cnt++;
+						continue;
+					}
+					q.offer(new int[]{time + 1, color, nx * m + ny});
+				}
+
+
+			}
+		}
+		answer = Math.max(answer, cnt);
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		g = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		arr = new int[n][m];
+		green = new int[g];
+		red = new int[r];
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				if (arr[i][j] == 2)
+					list.add(i * m + j);
+			}
+		}
+
+		answer = -1;
+		combinations(0, 0, 'R', new HashSet<>());
+		System.out.println(answer);
+	}
+}
