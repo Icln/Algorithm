84864df5bbf98b7c4ac3ee300cc475b75c3e697b
임강호 <55732952+Icln@users.noreply.github.com>
2025-04-07 01:35:18 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,33 +1,20 @@
 import java.util.*;
 class Solution {
-    static HashMap<String, Boolean> map = new HashMap<>();
     public int[] solution(int n, String[] words) {
-        int idx = 1;
-        int cnt = 1;
-        int num = 0;
-        for(String word : words){
-            if(map.get(word) == null){
-                map.put(word, true);
-                idx ++;
-            }
-            else{
-                return new int[]{idx, cnt};
-            }
-            
-            if(num == 0){
-                num += 1;
-                continue;
-            }
-            if(words[num - 1].charAt(words[num - 1].length() - 1) != word.charAt(0)){
-                return new int[]{idx - 1, cnt};
-            }
-            else{
-                if(idx > n){
-                    idx = 1;
-                    cnt += 1;
+        Set<String> usedWords = new HashSet<>();
+        for (int i = 0; i < words.length; i++) {
+            String current = words[i];
+            if (i > 0) {
+                String prev = words[i - 1];
+                if (prev.charAt(prev.length() - 1) != current.charAt(0)) {
+                    return new int[]{(i % n) + 1, (i / n) + 1};
                 }
             }
-            num += 1;
+
+            if (usedWords.contains(current)) {
+                return new int[]{(i % n) + 1, (i / n) + 1};
+            }
+            usedWords.add(current);
         }
         return new int[]{0, 0};
     }
