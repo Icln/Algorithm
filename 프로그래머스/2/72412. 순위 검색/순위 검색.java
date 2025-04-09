f77847459a62d5f9532b1eccb5b6e8ff4b5eca00
임강호 <55732952+Icln@users.noreply.github.com>
2025-04-09 23:25:33 +0900

    [level 2] Title: 순위 검색, Time: 861.16 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -1,58 +1,98 @@
 import java.util.*;
 class Solution {
-    public int[] solution(String[] info, String[] query) {
-        Map<String, List<Integer>> scoreMap = new HashMap<>();
-        
-        for (String applicant : info) {
-            String[] data = applicant.split(" ");
-            int score = Integer.parseInt(data[4]);
-            
-            generateCombinations(data, 0, "", scoreMap, score);
-        }
-        
-        for (List<Integer> scores : scoreMap.values()) {
-            Collections.sort(scores);
-        }
-        
-        int[] answer = new int[query.length];
-        for (int i = 0; i < query.length; i++) {
-            String q = query[i].replaceAll(" and ", " ");
-            String[] parts = q.split(" ");
-            String key = parts[0] + parts[1] + parts[2] + parts[3];
-            int targetScore = Integer.parseInt(parts[4]);
-            
-            List<Integer> scores = scoreMap.getOrDefault(key, new ArrayList<>());
-            answer[i] = countScoresAboveThreshold(scores, targetScore);
-        }
-        
-        return answer;
-    }
-    
-    private void generateCombinations(String[] data, int index, String current, Map<String, List<Integer>> scoreMap, int score) {
-        if (index == 4) {
-            scoreMap.computeIfAbsent(current, k -> new ArrayList<>()).add(score);
+    static HashMap<Integer, List<Integer>> map = new HashMap<>();
+    static HashMap<String, Integer> language = new HashMap<>();
+    static HashMap<String, Integer> position = new HashMap<>();
+    static HashMap<String, Integer> history = new HashMap<>();
+    static HashMap<String, Integer> food = new HashMap<>();
+    static void powerSet(int cnt, int idx, String l, String p, String h, String f, int cur, int score){
+        if(cnt == 4){
+            List<Integer> scores;
+            if(map.get(cur) == null)
+                scores = new ArrayList<>();
+            else
+                scores = map.get(cur);
+            scores.add(score);
+            map.put(cur, scores);
             return;
         }
         
-        generateCombinations(data, index + 1, current + data[index], scoreMap, score);
-        generateCombinations(data, index + 1, current + "-", scoreMap, score);
+        if(idx == 0){
+            powerSet(cnt + 1, idx + 1, l, p, h, f, cur + language.get(l), score);
+            powerSet(cnt + 1, idx + 1, l, p, h, f, cur + language.get("-"), score);    
+        }
+        else if(idx == 1){
+            powerSet(cnt + 1, idx + 1, l, p, h, f, cur + position.get(p), score);
+            powerSet(cnt + 1, idx + 1, l, p, h, f, cur + position.get("-"), score);    
+        }
+        else if(idx == 2){
+            powerSet(cnt + 1, idx + 1, l, p, h, f, cur + history.get(h), score);
+            powerSet(cnt + 1, idx + 1, l, p, h, f, cur + history.get("-"), score);    
+        }
+        else {
+            powerSet(cnt + 1, idx + 1, l, p, h, f, cur + food.get(f), score);
+            powerSet(cnt + 1, idx + 1, l, p, h, f, cur + food.get("-"), score);    
+        }
     }
-    
-    private int countScoresAboveThreshold(List<Integer> scores, int threshold) {
-        if (scores.isEmpty()) return 0;
+    static int binarySearch(List<Integer> tmp, int score){
+        int l = 0, r = tmp.size();
+        while(l < r){
+            int mid = (l + r) / 2;
+            if(tmp.get(mid) < score)
+                l = mid + 1;
+            else
+                r = mid;
+        }    
+        return tmp.size() - l;
+    } 
+  
+    public int[] solution(String[] info, String[] query) {
+        language.put("cpp", 1000);
+        language.put("java", 2000);
+        language.put("python", 3000);
+        language.put("-", 4000);
         
-        int left = 0;
-        int right = scores.size();
+        position.put("backend", 100);
+        position.put("frontend", 200);
+        position.put("-", 400);
         
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (scores.get(mid) < threshold) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
+        
+        history.put("junior", 10);
+        history.put("senior", 20);
+        history.put("-", 40);
+        
+        food.put("chicken", 1);
+        food.put("pizza", 2);
+        food.put("-", 4);
+        
+        for(String i : info){
+            String[] tmp = i.split(" ");
+            Integer key = language.get(tmp[0]) + position.get(tmp[1]) + history.get(tmp[2]) + food.get(tmp[3]);
+            
+            powerSet(0, 0, tmp[0], tmp[1], tmp[2], tmp[3], 0, Integer.parseInt(tmp[4]));        
         }
         
-        return scores.size() - left;
+        for (List<Integer> value : map.values()) {
+            Collections.sort(value); 
+        }
+        
+        int[] answer = new int[query.length];
+        int i = 0;
+        for(String q : query){
+            String[] condition = q.split(" ");
+            Integer tmp = 0;
+            tmp += language.get(condition[0]);
+            tmp += position.get(condition[2]);
+            tmp += history.get(condition[4]);
+            tmp += food.get(condition[6]);
+            
+            if(map.get(tmp) == null){
+                answer[i++] = 0;
+                continue;
+            }
+            List<Integer> arr = map.get(tmp);
+            answer[i++] = binarySearch(arr, Integer.parseInt(condition[7]));
+        }
+        return answer;
     }
 }
