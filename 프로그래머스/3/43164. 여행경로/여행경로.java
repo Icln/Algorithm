8750d98005395df43499922d58d851bdd9146e67
임강호 <55732952+Icln@users.noreply.github.com>
2025-04-10 22:18:08 +0900

    [level 3] Title: 여행경로, Time: 97.54 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,56 +1,37 @@
 import java.util.*;
 class Solution {
+    static List<String> list = new ArrayList<>();
     static List<Integer> idx = new ArrayList<>();
-    static List<String> strings = new ArrayList<>();
-    static List<String[]> answers = new ArrayList<>();
-    static HashMap<String, Integer> map = new HashMap<>();
     static boolean[] visited;
-    static int[] order; 
-    static int len;
 
-    static void dfs(int cnt, int prev, String[][] tickets){
-        if(cnt == len){
-            String[] tmp = new String[len + 1]; 
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < len; i++){
-                tmp[i] = tickets[order[i]][0];
-                sb.append(tmp[i]);
-                if(i == len - 1)
-                    tmp[i + 1] = tickets[order[i]][1];
-            }
-            
-            map.put(sb.toString(), answers.size());
-            strings.add(sb.toString());
-            answers.add(tmp);
+    static void dfs(int cnt, int prev, String[][] tickets, String s){
+        if(cnt == tickets.length){ 
+            list.add(s + " " + tickets[prev][1]);
             return;
         }
-        for(int i = 0; i < len; i++){
+        for(int i = 0; i < tickets.length; i++){
             if(!visited[i] && tickets[prev][1].equals(tickets[i][0])){
                 visited[i] = true;
-                order[cnt] = i;
-                dfs(cnt + 1, i, tickets);
+                dfs(cnt + 1, i, tickets, s + " " + tickets[prev][1]);
                 visited[i] = false;
             }
         }
     }
     
     public String[] solution(String[][] tickets) {
-        len = tickets.length;
-        for(int i = 0; i < len; i++){
-            if(tickets[i][0].equals("ICN")){
+        for(int i = 0; i < tickets.length; i++){
+            if(tickets[i][0].equals("ICN"))
                 idx.add(i);
-            }
         }
         
-        order = new int[len];
-        visited = new boolean[len];
+        visited = new boolean[tickets.length];
         for(int start: idx){
             visited[start] = true;
-            order[0] = start;
-            dfs(1, start, tickets);
+            dfs(1, start, tickets, "ICN");
             visited[start] = false;
         }
-        Collections.sort(strings);
-        return answers.get(map.get(strings.get(0)));
+        
+        Collections.sort(list);
+        return list.get(0).split(" ");
     }
 }
